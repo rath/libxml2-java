0286d0be1f35dcbb04d36e868a1ff545f8e005af
Jang-Ho Hwang <rath@xrath.com>
2013-11-07 22:14:50 +0000

    Exception class for libxml2 internal errors

---
+++ src/main/java/rath/libxml/LibXmlInternalException.java
@@ -0,0 +1,16 @@
+package rath.libxml;
+
+/**
+ * User: rath
+ * Date: 07/11/2013
+ * Time: 21:41
+ */
+public class LibXmlInternalException extends RuntimeException {
+	public LibXmlInternalException() {
+
+	}
+
+	public LibXmlInternalException(String message) {
+		super(message);
+	}
+}
