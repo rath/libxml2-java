6ed946891a608109e1952ccf413ed127a549bfa9
Jang-Ho Hwang <rath@xrath.com>
2013-11-22 18:46:50 +0000

    Added SAX version of RssTest

--- src/test/java/org/xmlsoft/test/RssTest.java
+++ src/test/java/org/xmlsoft/test/RssTest.java
@@ -6,12 +6,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.w3c.dom.*;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
 import org.xmlsoft.*;
 import org.xmlsoft.Document;
 import org.xmlsoft.Node;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
@@ -114,6 +118,122 @@ public class RssTest {
 		printReport("xercesj");
 	}
 
+	@Test
+	public void testWithLibXmlSAX() throws Exception {
+		SAXHandler handler = new SAXAdapter() {
+			boolean underItem = false;
+			boolean underTitle = false;
+			boolean underLink = false;
+			String title;
+			String link;
+			@Override
+			public void startElement(String uri, String localName, String qName, Attributes atts) {
+				if(localName.equals("item")) {
+					underItem = true;
+				}
+				else if(localName.equals("title")) {
+					underTitle = true;
+				}
+				else if(localName.equals("link")) {
+					underLink = true;
+				}
+			}
+
+			@Override
+			public void characters(char[] ch, int start, int length) {
+				if(underTitle) {
+					title = new String(ch, start, length);
+				} else if(underLink) {
+					link = new String(ch, start, length);
+				}
+			}
+
+			@Override
+			public void endElement(String uri, String localName, String qName) {
+				if(localName.equals("item")) {
+					underItem = false;
+					RssItem item = RssItem.build(title, link);
+				}
+				else if(localName.equals("title")) {
+					underTitle = false;
+				}
+				else if(localName.equals("link")) {
+					underLink = false;
+				}
+			}
+		};
+
+		SAXHandlerEngine engine = new SAXHandlerEngine();
+		engine.setAwarePrefixMapping(false);
+		File sampleFile = new File("sample-xmls/rss-infoq.xml");
+		for (int i = 0; i < 50; i++) {
+			startWatch();
+			LibXml.parseSAX(sampleFile, handler, false, engine);
+			stopWatch();
+		}
+		printReport("libxml2 SAX");
+		LibXml.printTcmallocStat();
+	}
+
+	@Test
+	public void testWithJAXPSAX() throws Exception {
+		DefaultHandler handler = new DefaultHandler() {
+			boolean underItem = false;
+			boolean underTitle = false;
+			boolean underLink = false;
+			String title;
+			String link;
+			@Override
+			public void startElement(String uri, String localName, String qName, Attributes atts) {
+				if(qName.equals("item")) {
+					underItem = true;
+				}
+				else if(qName.equals("title")) {
+					underTitle = true;
+				}
+				else if(localName.equals("link")) {
+					underLink = true;
+				}
+			}
+
+			@Override
+			public void characters(char[] ch, int start, int length) {
+				if(underTitle) {
+					title = new String(ch, start, length);
+				} else if(underLink) {
+					link = new String(ch, start, length);
+				}
+			}
+
+			@Override
+			public void endElement(String uri, String localName, String qName) {
+				if(qName.equals("item")) {
+					underItem = false;
+					RssItem item = RssItem.build(title, link);
+				}
+				else if(qName.equals("title")) {
+					underTitle = false;
+				}
+				else if(qName.equals("link")) {
+					underLink = false;
+				}
+			}
+		};
+
+		File sampleFile = new File("sample-xmls/rss-infoq.xml");
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		SAXParser parser = factory.newSAXParser();
+
+		for (int i = 0; i < 50; i++) {
+			startWatch();
+			parser.parse(sampleFile, handler);
+			stopWatch();
+		}
+		printReport("xercesj SAX");
+		LibXml.printTcmallocStat();
+	}
+
 	static class RssItem {
 		static DateFormat fmtDate = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz");
 		public String title;
@@ -122,6 +242,19 @@ public class RssTest {
 		public List<String> categories = new ArrayList<String>();
 		public Date pubDate;
 		public String guid;
+
+		/**
+		 * Simplify version for SAX parsing
+		 * @param title title of item
+		 * @param link link of item
+		 * @return item instance filled with name and title
+		 */
+		public static RssItem build(String title, String link) {
+			RssItem item = new RssItem();
+			item.title = title;
+			item.link = link;
+			return item;
+		}
 
 		public static RssItem build(Node node) throws ParseException {
 			RssItem item = new RssItem();
